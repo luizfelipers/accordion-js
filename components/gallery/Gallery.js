050bc45436a08bde64234fba8fa63247859b0ce5
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2020-11-11 21:31:55 -0300

    First Files from my Accordion component
    Still translating html and css into React Components, then to stateless components to use Hooks

---
+++ components/gallery/Gallery.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import './gallery.css'
+
+export default class Gallery extends Component {
+    render() {
+        return (
+            <div>
+                <div className='container'>
+
+
+<div class="containerzinho">
+                    <h1>Há 50 anos promovendo transporte de qualidade e confiança pelo Brasil.</h1>
+
+            
+                    <div class="gallery-wrap">
+                        <div class="item item-1">
+
+                            <h2>Turismo</h2>
+                            <div className='escondido'>
+                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis, felis tincidunt accumsan commodo, leo nisi commodo felis, a egestas arcu nunc in nunc. Etiam interdum massa ac augue molestie, et accumsan ex sagittis. Morbi imperdiet massa vel odio blandit, ac suscipit dui mollis. Ut bibendum turpis sed ligula efficitur lobortis. Integer finibus ultrices est, a lobortis odio varius id. Ut ut facilisis nunc. Nam tempus turpis vitae purus iaculis fermentum.</p>
+                                <button>Ver mais</button>
+                                
+                                
+                                </div>
+                            
+
+                        </div>
+                        <div class="item item-2">
+
+                            <h2>Executivo</h2>
+
+                            <div className='escondido'>
+                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis, felis tincidunt accumsan commodo, leo nisi commodo felis, a egestas arcu nunc in nunc. Etiam interdum massa ac augue molestie, et accumsan ex sagittis. Morbi imperdiet massa vel odio blandit, ac suscipit dui mollis. Ut bibendum turpis sed ligula efficitur lobortis. Integer finibus ultrices est, a lobortis odio varius id. Ut ut facilisis nunc. Nam tempus turpis vitae purus iaculis fermentum.</p>
+                                <button>Ver mais</button>
+                                
+                                
+                                </div>
+                            
+
+                        </div>
+                        <div class="item item-3">
+                            <h2>Urbano</h2>
+
+                            <div className='escondido'>
+                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis, felis tincidunt accumsan commodo, leo nisi commodo felis, a egestas arcu nunc in nunc. Etiam interdum massa ac augue molestie, et accumsan ex sagittis. Morbi imperdiet massa vel odio blandit, ac suscipit dui mollis. Ut bibendum turpis sed ligula efficitur lobortis. Integer finibus ultrices est, a lobortis odio varius id. Ut ut facilisis nunc. Nam tempus turpis vitae purus iaculis fermentum.</p>
+                                <button>Ver mais</button>
+                                
+                                
+                                </div>
+                            
+
+                        </div>
+                        <div class="item item-4"><h2>Eventos</h2>
+
+                        <div className='escondido'>
+                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis, felis tincidunt accumsan commodo, leo nisi commodo felis, a egestas arcu nunc in nunc. Etiam interdum massa ac augue molestie, et accumsan ex sagittis. Morbi imperdiet massa vel odio blandit, ac suscipit dui mollis. Ut bibendum turpis sed ligula efficitur lobortis. Integer finibus ultrices est, a lobortis odio varius id. Ut ut facilisis nunc. Nam tempus turpis vitae purus iaculis fermentum.</p>
+                                <button>Ver mais</button>
+                                
+                                
+                                </div>
+
+                            </div>
+                        <div class="item item-5">
+                            <h2>Fretamento</h2>
+                            <div className='escondido'>
+                                
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis, felis tincidunt accumsan commodo, leo nisi commodo felis, a egestas arcu nunc in nunc. Etiam interdum massa ac augue molestie, et accumsan ex sagittis. Morbi imperdiet massa vel odio blandit, ac suscipit dui mollis. Ut bibendum turpis sed ligula efficitur lobortis. Integer finibus ultrices est, a lobortis odio varius id. Ut ut facilisis nunc. Nam tempus turpis vitae purus iaculis fermentum.</p>
+                                <button>Ver mais</button>
+                                
+                                
+                                </div>
+                            
+                        </div>
+                      
+                    </div>
+                </div>
+
+
+
+
+
+                </div>
+                
+            </div>
+        )
+    }
+}
